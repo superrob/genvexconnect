93ae04fc4c1508217521fc7cf9cc9b1ba522d273
Robin Madsen <robin@robserob.dk>
2024-07-21 22:58:43 +0200

    CTS602 Added anti legionella

--- custom_components/genvex_connect/select.py
+++ custom_components/genvex_connect/select.py
@@ -14,7 +14,10 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     new_entities= []
     if genvexNabto.providesValue(GenvexNabtoSetpointKey.FAN_SPEED):
         new_entities.append(GenvexConnectSelectFanLevel(genvexNabto, GenvexNabtoSetpointKey.FAN_SPEED))
+    if genvexNabto.providesValue(GenvexNabtoSetpointKey.ANTILEGIONELLA_DAY):
+        new_entities.append(GenvexConnectSelectAntilegionellaDay(genvexNabto, GenvexNabtoSetpointKey.ANTILEGIONELLA_DAY))
         
+
     async_add_entities(new_entities)
         
 
@@ -40,14 +43,33 @@ class GenvexConnectSelectFanLevel(GenvexConnectEntityBase, SelectEntity):
     async def async_select_option(self, option: str) -> None:
         """Change the selected option."""
         fanLevel = 0
-        if option == "Level 1":
-            fanLevel = 1
-        elif option == "Level 2":
-            fanLevel = 2
-        elif option == "Level 3":
-            fanLevel = 3
-        elif option == "Level 4":
-            fanLevel = 4
+        if option in self._attr_options:
+            fanLevel = self._attr_options.index(option)
         self.genvexNabto.setSetpoint(self._valueKey, fanLevel)
 
+class GenvexConnectSelectAntilegionellaDay(GenvexConnectEntityBase, SelectEntity):
+    def __init__(self, genvexNabto, valueKey):
+        super().__init__(genvexNabto, valueKey, valueKey)
+        self._valueKey = valueKey
+        self._attr_options = ["Off", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+    @property
+    def icon(self):
+        """Return the icon of the sensor."""
+        return "mdi:bacteria"
+    
+    @property
+    def current_option(self) -> str | None:
+        """Return the selected entity option to represent the entity state."""
+        currentValue = int(self.genvexNabto.getValue(self._valueKey))
+        if currentValue < 0 or currentValue > 7:
+            return "Off"
+        return self._attr_options[currentValue]
+
+    async def async_select_option(self, option: str) -> None:
+        """Change the selected option."""
+        day = 0
+        if option in self._attr_options:
+            day = self._attr_options.index(option)
+        self.genvexNabto.setSetpoint(self._valueKey, day)
     
