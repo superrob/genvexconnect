b6d9b0621b174fb282e84449522c7275e439a035
Robin Madsen <robin@robserob.dk>
2026-09-14 17:09:51 +0200

    CTS400 - Implements humidity settings as requested in #56

--- custom_components/genvex_connect/number.py
+++ custom_components/genvex_connect/number.py
@@ -4,7 +4,7 @@ from homeassistant.helpers.entity import Entity
 from homeassistant.components.number import NumberDeviceClass, NumberEntity
 from genvexnabto import GenvexNabto, GenvexNabto, GenvexNabtoSetpointKey
 from .entity import GenvexConnectEntityBase
-from homeassistant.const import EntityCategory
+from homeassistant.const import EntityCategory, UnitOfTemperature
 from .const import DOMAIN
 
 
@@ -14,13 +14,13 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 
     new_entities = []
     if genvexNabto.providesValue(GenvexNabtoSetpointKey.BYPASS_OPENOFFSET):
-        new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.BYPASS_OPENOFFSET))
+        new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.BYPASS_OPENOFFSET, deviceClass=NumberDeviceClass.TEMPERATURE, unitOfMessurement=UnitOfTemperature.CELSIUS))
     if genvexNabto.providesValue(GenvexNabtoSetpointKey.BYPASS_TURNOFF):
-        new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.BYPASS_TURNOFF))
+        new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.BYPASS_TURNOFF, deviceClass=NumberDeviceClass.TEMPERATURE, unitOfMessurement=UnitOfTemperature.CELSIUS))
     if genvexNabto.providesValue(GenvexNabtoSetpointKey.BYPASS_FORCE_SPEED):
-        new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.BYPASS_FORCE_SPEED))
+        new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.BYPASS_FORCE_SPEED, unitOfMessurement="%"))
     if genvexNabto.providesValue(GenvexNabtoSetpointKey.BYPASS_FORCE_TEMP):
-        new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.BYPASS_FORCE_TEMP))
+        new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.BYPASS_FORCE_TEMP, deviceClass=NumberDeviceClass.TEMPERATURE, unitOfMessurement=UnitOfTemperature.CELSIUS))
     # Air supply level sliders
     if genvexNabto.providesValue(GenvexNabtoSetpointKey.SUPPLY_AIR_LEVEL1):
         new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.SUPPLY_AIR_LEVEL1, defaultEnabled=False))
@@ -40,20 +40,20 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
         new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.EXTRACT_AIR_LEVEL4, defaultEnabled=False))
     # Boost time
     if genvexNabto.providesValue(GenvexNabtoSetpointKey.BOOST_TIME):
-        new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.BOOST_TIME))
+        new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.BOOST_TIME, deviceClass=NumberDeviceClass.DURATION, unitOfMessurement="min"))
     if genvexNabto.providesValue(GenvexNabtoSetpointKey.HOTWATER_TEMP):
-        new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.HOTWATER_TEMP))
+        new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.HOTWATER_TEMP, deviceClass=NumberDeviceClass.TEMPERATURE, unitOfMessurement=UnitOfTemperature.CELSIUS))
     if genvexNabto.providesValue(GenvexNabtoSetpointKey.HOTWATER_BOOSTTEMP):
-        new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.HOTWATER_BOOSTTEMP))
+        new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.HOTWATER_BOOSTTEMP, deviceClass=NumberDeviceClass.TEMPERATURE, unitOfMessurement=UnitOfTemperature.CELSIUS))
     if genvexNabto.providesValue(GenvexNabtoSetpointKey.FILTER_DAYS_SETTING):
-        new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.FILTER_DAYS_SETTING))
+        new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.FILTER_DAYS_SETTING, deviceClass=NumberDeviceClass.DURATION, unitOfMessurement="d"))
     if genvexNabto.providesValue(GenvexNabtoSetpointKey.FILTER_MONTHS_SETTING):
         new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.FILTER_MONTHS_SETTING))
 
     if genvexNabto.providesValue(GenvexNabtoSetpointKey.SUPPLYAIR_MIN_TEMP_SUMMER):
-        new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.SUPPLYAIR_MIN_TEMP_SUMMER))
+        new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.SUPPLYAIR_MIN_TEMP_SUMMER, deviceClass=NumberDeviceClass.TEMPERATURE, unitOfMessurement=UnitOfTemperature.CELSIUS))
     if genvexNabto.providesValue(GenvexNabtoSetpointKey.SUPPLYAIR_MAX_TEMP_SUMMER):
-        new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.SUPPLYAIR_MAX_TEMP_SUMMER))
+        new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.SUPPLYAIR_MAX_TEMP_SUMMER, deviceClass=NumberDeviceClass.TEMPERATURE, unitOfMessurement=UnitOfTemperature.CELSIUS))
 
     if genvexNabto.providesValue(GenvexNabtoSetpointKey.COOLING_TEMPERATURE):
         new_entities.append(GenvexConnectNumberSetpointTemperature(genvexNabto, GenvexNabtoSetpointKey.COOLING_TEMPERATURE))
@@ -61,18 +61,26 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
         new_entities.append(GenvexConnectNumberSetpointTemperature(genvexNabto, GenvexNabtoSetpointKey.TEMP_SETPOINT))
 
     if genvexNabto.providesValue(GenvexNabtoSetpointKey.CENTRALHEAT_SUPPLY_MIN):
-        new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.CENTRALHEAT_SUPPLY_MIN))
+        new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.CENTRALHEAT_SUPPLY_MIN, deviceClass=NumberDeviceClass.TEMPERATURE, unitOfMessurement=UnitOfTemperature.CELSIUS))
     if genvexNabto.providesValue(GenvexNabtoSetpointKey.CENTRALHEAT_SUPPLY_MAX):
-        new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.CENTRALHEAT_SUPPLY_MAX))
+        new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.CENTRALHEAT_SUPPLY_MAX, deviceClass=NumberDeviceClass.TEMPERATURE, unitOfMessurement=UnitOfTemperature.CELSIUS))
+
+    if genvexNabto.providesValue(GenvexNabtoSetpointKey.CTS400_HUMIDITY_LOW_LEVEL):
+            new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.CTS400_HUMIDITY_LOW_LEVEL, deviceClass=NumberDeviceClass.HUMIDITY, unitOfMessurement="%"))
+    if genvexNabto.providesValue(GenvexNabtoSetpointKey.CTS400_HUMIDITY_HIGH_MAX_TIME):
+            new_entities.append(GenvexConnectNumber(genvexNabto, GenvexNabtoSetpointKey.CTS400_HUMIDITY_HIGH_MAX_TIME, deviceClass=NumberDeviceClass.DURATION, unitOfMessurement="min"))
 
     async_add_entities(new_entities)
 
 
 class GenvexConnectNumber(GenvexConnectEntityBase, NumberEntity):
-    def __init__(self, genvexNabto, valueKey, defaultEnabled=True):
+    def __init__(self, genvexNabto, valueKey, defaultEnabled=True, deviceClass:NumberDeviceClass|bool=False, unitOfMessurement:UnitOfTemperature|str|bool=False):
         super().__init__(genvexNabto, valueKey, valueKey)
         self._valueKey = valueKey
-        self._attr_device_class = NumberDeviceClass.TEMPERATURE
+        if deviceClass:
+            self._attr_device_class = deviceClass
+        if unitOfMessurement is not False:
+            self._attr_unit_of_measurement = unitOfMessurement
         self._attr_native_min_value = genvexNabto.getSetpointMinValue(valueKey)
         self._attr_native_max_value = genvexNabto.getSetpointMaxValue(valueKey)
         self._attr_native_step = genvexNabto.getSetpointStep(valueKey)
@@ -93,6 +101,7 @@ class GenvexConnectNumberSetpointTemperature(GenvexConnectEntityBase, NumberEnti
         super().__init__(genvexNabto, f"{valueKey}_slider", valueKey)
         self._valueKey = valueKey
         self._attr_device_class = NumberDeviceClass.TEMPERATURE
+        self._attr_unit_of_measurement = UnitOfTemperature.CELSIUS
         self._attr_native_min_value = genvexNabto.getSetpointMinValue(valueKey)
         self._attr_native_max_value = genvexNabto.getSetpointMaxValue(valueKey)
         self._attr_native_step = genvexNabto.getSetpointStep(valueKey)
--- custom_components/genvex_connect/select.py
+++ custom_components/genvex_connect/select.py
@@ -2,8 +2,10 @@
 
 from homeassistant.helpers.entity import Entity
 from homeassistant.components.select import SelectEntity
+from homeassistant.const import EntityCategory
 from genvexnabto import GenvexNabto, GenvexNabtoDatapointKey, GenvexNabtoSetpointKey
 from .entity import GenvexConnectEntityBase
+from typing import List, Dict
 
 from .const import DOMAIN
 
@@ -30,6 +32,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
                     "sunday",
                 ],
                 "mdi:bacteria",
+                category=EntityCategory.CONFIG
             )
         )
     if genvexNabto.providesValue(GenvexNabtoSetpointKey.COOLING_PRIORITY):
@@ -69,15 +72,30 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
             )
         )
 
+    if genvexNabto.providesValue(GenvexNabtoSetpointKey.CTS400_HUMIDITY_LOW_STEP):
+            new_entities.append(
+                GenvexConnectSelectDefinedValues(
+                    genvexNabto, GenvexNabtoSetpointKey.CTS400_HUMIDITY_LOW_STEP, {0: "0", 1: "1", 2: "2", 3: "3"}, "mdi:fan", category=EntityCategory.CONFIG
+                )
+            )
+    if genvexNabto.providesValue(GenvexNabtoSetpointKey.CTS400_HUMIDITY_HIGH_STEP):
+            new_entities.append(
+                GenvexConnectSelectDefinedValues(
+                    genvexNabto, GenvexNabtoSetpointKey.CTS400_HUMIDITY_HIGH_STEP, {2: "2", 3: "3", 4: "4"}, "mdi:fan", category=EntityCategory.CONFIG
+                )
+            )
+            
     async_add_entities(new_entities)
 
 
 class GenvexConnectSelectGeneric(GenvexConnectEntityBase, SelectEntity):
-    def __init__(self, genvexNabto, valueKey, options, icon):
+    def __init__(self, genvexNabto, valueKey, options:List[str], icon:str, category:EntityCategory|None=None, optionValues:List[int]|bool=False):
         super().__init__(genvexNabto, valueKey, valueKey)
         self._valueKey = valueKey
         self._attr_options = options
         self._icon = icon
+        self._attr_entity_category = category
+        self._optionValues = optionValues
 
     @property
     def icon(self):
@@ -97,4 +115,34 @@ class GenvexConnectSelectGeneric(GenvexConnectEntityBase, SelectEntity):
         chosenValue = 0
         if option in self._attr_options:
             chosenValue = self._attr_options.index(option)
+        self.genvexNabto.setSetpoint(self._valueKey, chosenValue)
+
+class GenvexConnectSelectDefinedValues(GenvexConnectEntityBase, SelectEntity):
+    def __init__(self, genvexNabto, valueKey, options:Dict[int, str], icon:str, category:EntityCategory|None=None):
+        super().__init__(genvexNabto, valueKey, valueKey)
+        self._valueKey = valueKey
+        self._options_keyed = options
+        self._attr_options = list(options.values())
+        self._icon = icon
+        self._attr_entity_category = category
+
+    @property
+    def icon(self):
+        """Return the icon of the sensor."""
+        return self._icon
+
+    @property
+    def current_option(self) -> str | None:
+        """Return the selected entity option to represent the entity state."""
+        currentValue = int(self.genvexNabto.getValue(self._valueKey))
+        if currentValue not in self._options_keyed:
+            return self._attr_options[0]        
+        return self._attr_options[list(self._options_keyed.keys()).index(currentValue)]
+
+    async def async_select_option(self, option: str) -> None:
+        """Change the selected option."""
+        chosenValue = 0
+        if option in self._attr_options:
+            chosenIndex = self._attr_options.index(option)
+            chosenValue = list(self._options_keyed.keys())[chosenIndex]
         self.genvexNabto.setSetpoint(self._valueKey, chosenValue)
